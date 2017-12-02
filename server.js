b6d4174211e2bf46d4e3528d9f142c6407901bd1
Elias Khattar <khattarelias@gmail.com>
2017-12-02 20:43:48 +0000

    connected firebase

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express')
+var firebase = require("firebase")
 const app = express()
 console.log("test")
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT ,process.env.IP, () => console.log('Example app listening on port 3000!'))
+
+var config = {
+    apiKey: "AIzaSyDigiT3cpa-qZL8lFQWA5dqRD_Q8W5cDos",
+    authDomain: "project14-3.firebaseapp.com",
+    databaseURL: "https://project14-3.firebaseio.com",
+    storageBucket: "project14-3.appspot.com",
+  };
+  firebase.initializeApp(config);
+  
+   // Get a reference to the database service
+  var database = firebase.database();
