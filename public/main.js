b304eff3a567c3bb0ed7bfa7c35f074d4d364bac
Elias Khattar <khattarelias@gmail.com>
2017-12-03 13:24:41 +0000

    Created public directory as web root

---
+++ public/main.js
@@ -0,0 +1,9 @@
+// var firebase = require("firebase")
+
+
+//how to load a json file into a javascript variable
+var data = {};
+console.log("hello")
+
+// //iterate through the data dictionary -> for each question under questions
+// $("#show-questions").addClass("question-list");
