029b4ff62c3abee0a7641a9137634f5afdc860ef
Elias Khattar <khattarelias@gmail.com>
2017-12-04 09:35:18 +0000

    moved event from public to own folder

---
+++ event/event.js
@@ -0,0 +1,26 @@
+angular.module('p3')
+.controller('eventPageController', function($scope, $location, $routeParams, eventService, userService){
+    var eventId = parseInt($routeParams.eventId);
+
+    $scope.newQuestion = '';
+    $scope.event = eventService.getEvent(eventId);
+
+    $scope.addQuestion= function(){ 
+        if(!$scope.newQuestion){
+            return false;
+        }       
+
+        var currentUser = userService.getLoggedInUser();
+
+        var newQuestion = {
+            text: $scope.newQuestion,
+            username: currentUser.name
+        };
+
+        $scope.event.questions.push(newQuestion);
+    
+        $scope.newQuestion = '';
+    }
+
+
+});
