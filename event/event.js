0ac5a111a387a430a29d5c9d0b971fda04a5271b
Elias Khattar <khattarelias@gmail.com>
2017-12-07 01:51:19 +0000

    Successfully adding questions to db and page

--- event/event.js
+++ event/event.js
@@ -6,14 +6,8 @@ angular.module('p3')
 
     $scope.newQuestion = '';
     $scope.event = eventService.getEvent(eventId);
-    var question_count;
-    if($scope.event.questions){
-        question_count = $scope.event.questions.length;
-    } else {
-        question_count = 0;
-        $scope.event.questions = {};
-    }
-    
+    var question_count = 0;
+    console.log($scope.event)
 
     $scope.addQuestion= function(){ 
         if(!$scope.newQuestion){
@@ -28,20 +22,73 @@ angular.module('p3')
         };
         
         console.log($scope.event)
+        var set_count = false;
+        for (key in $scope.event) {
+            if (!$scope.event.hasOwnProperty(key)) continue;
+            console.log("KEY: " + key);
+            // console.log($scope.event[key]);
+            if(key == "questions"){
+                // console.log($scope.event[key])
+                console.log("Questions present")
+                var length = $scope.event[key].length
+                var count = 0, i = length;
+                while (i--) {
+                    if (typeof $scope.event[key][i] === "undefined") {count ++;}
+                }
+                // console.log(length)
+                // console.log("empties: " + count)
+                question_count = length - count;
+                var set_count = true;
+            } else {
+                // console.log("Questions not found")
+                if(set_count == false){
+                    question_count = 0;
+                }
+                
+            }
+        }
+        console.log("Num existing qs: " + question_count)
+        
         var questionId = question_count + 1;
-        var questionObj = {};
-        questionObj[questionId] = newQuestion;
 
-        $scope.event.questions =questionObj;
+       
+        // console.log($scope.event)
+        if(question_count -1 == 0){
+            console.log("This is the 1st question")
+        }
+        $scope.event.questions[questionId] = newQuestion;
         var updateEvent = {};
         updateEvent["title"] = $scope.event.title;
         updateEvent["username"] = $scope.event.username;
         updateEvent["questions"] = $scope.event.questions;
-
+        
+        // delete the haskey $$hashKey key
+        for(var i; i <= question_count; i++){
+            var current_item = updateEvent.questions[i];
+          
+            // console.log(typeof current_item)
+            if(typeof current_item == "object"){
+                console.log("Hoora")
+                console.log(current_item)
+                // angular.toJson(current_item)
+            }
+        }
+        // updateEvent.questions[0]
+        var sendToFB = angular.toJson(updateEvent.questions).replace("null,","");
+        // console.log(typeof sendToFB)
+        var finalSend = JSON.parse(sendToFB)
+        console.log(finalSend)
+        var new_dict = {};
+        for( item in finalSend){
+            console.log(item);
+            new_dict[item] = finalSend[item]
+        }
+        console.log(new_dict)
+        updateEvent.questions = new_dict;
+        console.log(updateEvent)
        
-        console.log($scope.event)
-        // $scope.event.questions.push(newQuestion);
         firebase.database().ref('/events/' + eventId).set(updateEvent)
+        
     
         $scope.newQuestion = '';
     }
